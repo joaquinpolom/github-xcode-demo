b466685597ef6070eea3eabbd3b7b7b98bd9123e
Joaquín Polo Molero <ximo20@hotmail.com>
2017-04-29 21:39:56 +0100

    Initial Commit

---
+++ TestGitHub/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestGitHub
+//
+//  Created by Joaquín Polo Molero on 29/4/17.
+//  Copyright © 2017 Joaquín Polo Molero. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestGitHub/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestGitHub
+//
+//  Created by Joaquín Polo Molero on 29/4/17.
+//  Copyright © 2017 Joaquín Polo Molero. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
